26a2111317613c0f42ce74caacd297fc15ed542c
KrystianCyga <259247@student.pwr.edu.pl>
2021-06-27 21:36:19 +0200

    poczatek

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/krystian/Documents/objektowo/zad5_3-KrystianCyga/tests
+# Build directory: /home/krystian/Documents/objektowo/zad5_3-KrystianCyga/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(program.unit_tests "/home/krystian/Documents/objektowo/zad5_3-KrystianCyga/build/unit_tests" "-V")
+set_tests_properties(program.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/krystian/Documents/objektowo/zad5_3-KrystianCyga/tests/CMakeLists.txt;26;add_test;/home/krystian/Documents/objektowo/zad5_3-KrystianCyga/tests/CMakeLists.txt;0;")
